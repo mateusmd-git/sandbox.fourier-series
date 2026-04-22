4ae3fb49cd50cf175fbf558f521d25a59d7164bd
mateusmd-git <mateusmduraes@gmail.com>
2026-04-21 22:13:37 -0300

    feat: main entry point

--- src/fourier_series/main.py
+++ src/fourier_series/main.py
@@ -1,4 +1,9 @@
+import logging
+
 import typer
+
+from fourier_series.animation import animate
+from fourier_series.logic import fourier_coeficient, sample_svg
 
 cli = typer.Typer()
 
@@ -11,18 +16,45 @@ def main(
         is_flag=True,
         help="Enable verbose mode.",
     ),
-    source: str = typer.Option(
-        None,
+    svg_file: str = typer.Option(
+        "",
         *["--source", "-i"],
         help="Target SVG file image.",
     ),
-    sample: int = typer.Option(
+    n_coeficients: int = typer.Option(
         50,
-        *["--sample", "-n"],
+        *["--coeficients", "-n"],
+        help="Number of coeficients calculated in the DFT.",
+    ),
+    samples: int = typer.Option(
+        2048,
+        *["--samples"],
         help="Number of samples.",
+    ),
+    num_frames: int = typer.Option(
+        600,
+        *["--frames"],
+        help="Number of frames per cicle.",
+    ),
+    theme: str = typer.Option(
+        "dark",
+        *["--color"],
+        help="Color theme betwen 'dark' and 'light'.",
+    ),
+    output_file: str | None = typer.Option(
+        None,
+        *["--save-as"],
+        help="Save the animation as a file.",
     ),
 ) -> int:
     """Main entry point."""
+    logging.basicConfig(level=logging.DEBUG if verbose else logging.WARNING)
+
+    z = sample_svg(svg_file, sample_num=samples)
+    coefs = fourier_coeficient(z, n_coefs=n_coeficients)
+
+    animate(coefs, frames=num_frames, theme=theme, save_as=output_file)
+
     return 0
 
 
